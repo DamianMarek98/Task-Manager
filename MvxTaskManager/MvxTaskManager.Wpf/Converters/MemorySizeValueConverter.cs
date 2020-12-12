a655a5aefc6ff5e6757884e49a498de8216220ac
Damian Marek <zmdddamian@gmail.com>
2020-12-12 13:51:24 +0100

    added converter

---
+++ MvxTaskManager/MvxTaskManager.Wpf/Converters/MemorySizeValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using MvvmCross.Converters;
+
+namespace MvxTaskManager.Wpf.Converters
+{
+    class MemorySizeValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long bytes = (long)value;
+            return $"{(bytes / 1024.00 / 1024.00).ToString("0.##")} MB";
+
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
